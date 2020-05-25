6372af485bdee992cc2d46163089a6e7373846b4
ozbe <1372945+ozbe@users.noreply.github.com>
2020-05-25 13:19:32 -0400

    5.5 The challenge

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -8,13 +8,25 @@ use aux5::{entry, prelude::*, Delay, Leds};
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
-    let half_period = 500_u16;
+    let half_period = 50_u16;
+    let mut index = 0;
+    let mut interval = 0;
 
     loop {
-        leds[0].on();
-        delay.delay_ms(half_period);
+        let next_index = (index + 1) % leds.len();
 
-        leds[0].off();
+        match interval {
+            0 => {
+                leds[index].on();
+                leds[next_index].on();
+            },
+            1 => {
+                leds[index].off();
+                index = next_index;
+            },
+            _ => {},
+        }
+        interval = (interval + 1) % 2;
         delay.delay_ms(half_period);
     }
 }
